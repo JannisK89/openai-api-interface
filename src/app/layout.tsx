6fbf9fa1483430c83f8f279104e93334a9928d39
JannisK89 <jannis.karanikis@gmail.com>
2024-08-03 12:46:15 +0200

    design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,37 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
+import { Manrope } from 'next/font/google'
+import clsx from 'clsx'
 
-const inter = Inter({ subsets: ['latin'] })
+const fontHeading = Manrope({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-heading',
+})
+
+const fontBody = Manrope({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-body',
+})
 
 export const metadata: Metadata = {
   title: 'OpenAI API Interface',
   description: 'TODO',
 }
 
-export default function RootLayout({
+export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={clsx('antialiased', fontHeading.variable, fontBody.variable)}
+      >
+        {children}
+      </body>
     </html>
   )
 }
