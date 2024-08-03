66a62b6f6f1d3c5e82519d19d13d169f314ade35
JannisK89 <jannis.karanikis@gmail.com>
2024-08-03 13:04:59 +0200

    tailwind fixes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,8 @@
 import { fontFamily } from 'tailwindcss/defaultTheme'
+import { Config } from 'tailwindcss/types/config'
 
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./components/**/*.{js,ts,jsx,tsx}', './app/**/*.{js,ts,jsx,tsx}'],
+const config: Config = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       fontFamily: {
@@ -50,21 +50,8 @@ export default {
         md: `calc(var(--radius) - 2px)`,
         sm: `calc(var(--radius) - 4px)`,
       },
-      keyframes: {
-        'accordion-down': {
-          from: { height: 0 },
-          to: { height: 'var(--radix-accordion-content-height)' },
-        },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: 0 },
-        },
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-      },
     },
   },
-  plugins: [require('tailwindcss-animate')],
 }
+
+export default config
